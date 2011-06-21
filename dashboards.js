6bcf49c0d0c064ee9fbfb12b2697eda4a0706976
Spencer Wallace <spencerw@email.arizona.edu>
2011-06-21 12:10:51 -0700

    modified:   dashboards.js 	public/gadgets/ASCIIDataLoader.html 	public/gadgets/histView.html

--- dashboards.js
+++ dashboards.js
@@ -36,41 +36,16 @@ var dashboards = [
   { attrs: { 
         id :3,
         author: 'Spencer',
-        name: 'ScalableScatterPlot' 
+        name: 'ScatterPlotAndHistogram' 
       },
       collections: {
         "gadgets": {  "models": [
-                      {"attrs":{"id":"skyView","url":"/gadgets/skyView.html","property":1,"longitude":-47.17500000000001,"latitude":11.8,"flySpeed":1}},
-                      {"attrs":{"id":"nameResolver","url":"/gadgets/nameResolver.html"}},{"attrs":{"id":"dataSetSelector","url":"/gadgets/dataSetSelector.html"}}]}
-      }
-  },
-<<<<<<< HEAD
-  {
-    id: 3,
-    author: 'Spencer',
-    name : 'histViewAndScatterPlot',
-    gadgets: [
-     {"id":"ASCIIDataLoader","url":"/gadgets/ASCIIDataLoader.html"},
-     {"id":"histView","url":"/gadgets/histView.html"},
-     {"id":"dataInquirer","url":"/gadgets/dataInquirer.html"},
-     {"id":"scalableScatter","url":"/gadgets/scalableScatter.html"},
-     {"id":"tableView","url":"/gadgets/tableView.html"}
-    ],
-    "dataSets": []
-=======
-  { attrs: { 
-        id: 4,
-        author: 'Spencer',
-        name: 'histView' 
-      },
-      collections: {
-        "gadgets": {  "models": [
-                      {"attrs":{"id":"histView","url":"/gadgets/histView.html"}},
+                      {"attrs":{"id":"ASCIIDataLoader","url":"/gadgets/ASCIIDataLoader.html"}},
                       {"attrs":{"id":"dataInquirer","url":"/gadgets/dataInquirer.html"}},
+                      {"attrs":{"id":"dataSetSelector","url":"/gadgets/dataSetSelector.html"}},
                       {"attrs":{"id":"scalableScatter","url":"/gadgets/scalableScatter.html"}},
-                      {"attrs":{"id":"tableView","url":"/gadgets/tableView.html"}}]}
+                      {"attrs":{"id":"histView","url":"/gadgets/histView.html"}}]}
       }
->>>>>>> 5283de1be53ce5d49799dc7141b9f4f4c3acb9c1
   }
 ];
 
