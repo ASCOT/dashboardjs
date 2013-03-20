e50e49ff002a704e94120562231d1d783e138bc1
Spencer Wallace <spencerw@email.arizona.edu>
2013-03-20 13:39:05 -0700

    Fixing various bugs

--- public/gadgets/gadgetsInfo.js
+++ public/gadgets/gadgetsInfo.js
@@ -1,17 +1,31 @@
 var gadgets = {
-  /*asciiFileLoader: {
-    name: "ASCII File Loader",
-    fileName: "asciiDataLoader.html",
-    description: "Load data into ASCOT from local text files.",
-    author: "Spencer Wallace - spencerw@email.arizona.edu",
-    version: "0.1"
-  },*/
   dataInquirer: { 
     name: "Data Inquirer",
     fileName: "dataInquirer.html",
     description: "Create DataSets from queries to web services and remote databases.",
     author: "Ian Smith - imsmith@uw.edu",
     version: "0.1" 
+  },
+  astroJsFitsViewer: { 
+    name: "AstroJS Fits Viewer",
+    fileName: "astroJsFitsViewer.html",
+    description: "Display and interact with fits files. Powered by Amit Kapadia's AstroJS FITS parser.",
+    author: "Amit Kapadia - amit@zooniverse.com, Spencer Wallace - spencerw@email.arizona.edu",
+    version: "0.1"
+  },
+  skyView: {
+    name: "Sky Viewer",
+    fileName: "skyView.html",
+    description: "Pan, zoom, and plot points across the sky with the Google Earth plugin.",
+    author: "Ian Smith - imsmith@uw.edu",
+    version: "0.1"
+  },
+  nameResolver: {
+    name: "Name Resolver",
+    fileName: "nameResolver.html",
+    description: "Resolve a celestial object name into its coordinates.",
+    author: "Ian Smith - imsmith@uw.edu",
+    version: "0.1"
   },
   dataSetSelector: {
      name: "DataSet Selector",
@@ -24,13 +38,6 @@ var gadgets = {
     name: "Histogram",
     fileName: "histogramView.html",
     description: "Create interactive histograms from DataSets.",
-    author: "Ian Smith - imsmith@uw.edu",
-    version: "0.1"
-  },
-  nameResolver: {
-    name: "Name Resolver",
-    fileName: "nameResolver.html",
-    description: "Resolve a celestial object name into its coordinates.",
     author: "Ian Smith - imsmith@uw.edu",
     version: "0.1"
   },
@@ -48,27 +55,14 @@ var gadgets = {
     author: "Spencer Wallace - spencerw@email.arizona.edu",
     version: "0.1"
   },
-  skyView: {
-    name: "Sky Viewer",
-    fileName: "skyView.html",
-    description: "Pan, zoom, and plot points across the sky with the Google Earth plugin.",
-    author: "Ian Smith - imsmith@uw.edu",
-    version: "0.1"
-  },
   tableView: { 
     name: "Table",
     fileName: "tableView.html",
     description: "Display DataSets as a table with movable and sortable columns.",
     author: "Ian Smith - imsmith@uw.edu",
     version: "0.1"
-  },
-  /*fitsViewer: { 
-    name: "Fits Viewer",
-    fileName: "fitsViewer.html",
-    description: "Display and interact with fits files.",
-    author: "Diego Marcos - diego.marcos@gmail.com, Spencer Wallace - spencerw@email.arizona.edu",
-    version: "0.1"
-  },
+  }
+  /*
   fitsViewer_PDR: { 
     name: "Fits Viewer PDR",
     fileName: "fitsViewer_PDR.html",
@@ -76,13 +70,6 @@ var gadgets = {
     author: "Diego Marcos - diego.marcos@gmail.com, Spencer Wallace - spencerw@email.arizona.edu",
     version: "0.1"
   },*/
-  astroJsFitsViewer: { 
-    name: "AstroJS Fits Viewer",
-    fileName: "astroJsFitsViewer.html",
-    description: "Display and interact with fits files. Powered by Amit Kapadia's AstroJS FITS parser.",
-    author: "Amit Kapadia - amit@zooniverse.com, Spencer Wallace - spencerw@email.arizona.edu",
-    version: "0.1"
-  }
 }
 
 module.exports.all = gadgets;
